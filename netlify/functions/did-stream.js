11e2cfedfae6d14782fc0a8fbd7e21ec707e3c7d
td01 <tddunn@gmail.com>
2026-06-06 11:35:59 +0100

    Add files via upload

--- netlify/functions/did-stream.js
+++ netlify/functions/did-stream.js
@@ -58,10 +58,11 @@ exports.handler = async function(event) {
           script: {
             type: 'text',
             input: text,
-        provider: {
-              type: 'microsoft',
-              voice_id: 'en-GB-SoniaNeural',
-            }
+            provider: {
+              type: 'elevenlabs',
+              voice_id: 'EQx6HGDYjkDpcli6vorJ',
+              model_id: 'eleven_turbo_v2_5',
+              voice_config: { stability: 0.4, similarity_boost: 0.75, style: 0.4 }
             }
           },
           session_id: sessionId,
